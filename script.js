62fd23759c8840bbaaf581607ac4bdd366a67110
Lukasz Suder <79355313+lukaszsuder@users.noreply.github.com>
2021-03-25 21:49:25 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ const resultWindow = document.querySelector(".result-window");
 const btnResult = document.querySelector(".check");
 const overlayBlur = document.querySelector(".overlay");
 const overlayContainer = document.querySelector(".overlay-container");
-const wpisz = document.querySelector(".wpisz");
+const containerMpg = document.querySelector(".container-mpg");
 const btnClose = document.querySelector(".close-window");
-const btnClose2 = document.querySelector(".close-window2");
+
 const radioKilo = document.querySelectorAll("input[name=distance]");
 const radioFuel = document.querySelectorAll("input[name=fuel]");
 const choiceMpg = document.querySelector("#choice-mpg");
 const choiceTime = document.querySelector("#choice-time");
 const counterWindow = document.querySelector(".counter");
+//Timer
+const timerWindow = document.querySelector(".counter-time");
+const overlayContainerTime = document.querySelector(".overlay-container-time");
+const containerTime = document.querySelector(".container-time");
+const btnResult2 = document.querySelector(".check2");
+const btnClose2 = document.querySelector(".close-window2");
+const getStart = document.querySelector(".start");
+const getFinish = document.querySelector(".finish");
+//******************************** M P G ***************************************//
 
 // First Choice
 // Open MPG  Window
@@ -21,12 +30,6 @@ const mpgWindow = function () {
 };
 choiceMpg.addEventListener("click", mpgWindow);
 
-// Open Time Window
-const timeWindow = function () {
-  overlayBlur.classList.remove("hidden");
-};
-choiceTime.addEventListener("click", timeWindow);
-
 //MPG Function
 function obliczMpg(x, y) {
   return x / y;
@@ -35,7 +38,7 @@ function obliczMpg(x, y) {
 // Result Window
 const showMpg = function () {
   // resultWindow.classList.remove("hidden");
-  wpisz.classList.add("hidden");
+  containerMpg.classList.add("hidden");
   overlayContainer.classList.remove("hidden");
 
   // DISTANCE. Take data
@@ -83,17 +86,40 @@ const closeMpg = function () {
   overlayContainer.classList.add("hidden");
   overlayBlur.classList.add("hidden");
   counterWindow.classList.add("hidden");
-  wpisz.classList.remove("hidden");
+  containerMpg.classList.remove("hidden");
 };
-const closeMpg2 = function () {
-  //resultWindow.classList.add("hidden");
-  overlayContainer.classList.add("hidden");
-  overlayBlur.classList.add("hidden");
-  counterWindow.classList.add("hidden");
-};
+
 btnClose.addEventListener("click", closeMpg);
 btnResult.addEventListener("click", showMpg);
 
 // END MPG
 
 // START TIME
+//******************************** TIMER ***************************************//
+// Open Time Window
+const timeWindow = function () {
+  timerWindow.classList.remove("hidden");
+  overlayBlur.classList.remove("hidden");
+};
+choiceTime.addEventListener("click", timeWindow);
+
+const ileGodzin = function () {
+  containerTime.classList.add("hidden");
+  overlayContainerTime.classList.remove("hidden");
+  console.log("Mamy");
+  const minusBreak = getFinish.value - getStart.value - 0.5;
+  const totalworked = getFinish.value - getStart.value;
+  document.querySelector(".show-total").textContent = totalworked;
+  document.querySelector(".show-break").textContent = minusBreak;
+};
+
+const closeTimer = function () {
+  //resultWindow.classList.add("hidden");
+  overlayContainerTime.classList.add("hidden");
+  overlayBlur.classList.add("hidden");
+  timerWindow.classList.add("hidden");
+  containerTime.classList.remove("hidden");
+};
+
+btnClose2.addEventListener("click", closeTimer);
+btnResult2.addEventListener("click", ileGodzin);
